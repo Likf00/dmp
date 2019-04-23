ab5620acc464dc969af7538576faae3e27e1f97f
yimting <yimting@yeah.net>
2019-04-23 20:56:52 +0800

    ceshi

---
+++ src/main/scala/com/ylqz/dmp/tools/one.scala
@@ -0,0 +1,12 @@
+package com.ylqz.dmp.tools
+
+/**
+  * @ClassName one.scala
+  * @author yimting
+  * @version 1.0.0
+  * @Description TODO
+  * @createTime 2019年04月23日 20:55:00
+  */
+object one {
+
+}
